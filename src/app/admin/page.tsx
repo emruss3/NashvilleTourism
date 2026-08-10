8f9ff709621aa92047caf4c7e06e38b2eca5ec58
emruss3 <emruss3@gmail.com>
2026-08-09 21:36:02 -0500

    Add admin landing redirect

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
+export const metadata = { robots: { index: false, follow: false } };
+
+export default function AdminPage() {
+  redirect('/admin/experiences');
+}
